49cd1fc43c3f99a561958624d06e155eaf526647
Ned-el-ch <nedelchev.niki@gmail.com>
2019-10-28 09:42:57 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,30 +1,70 @@
 class Application
 
-  @@items = ["Apples","Carrots","Pears"]
+    @@items = ["Apples","Carrots","Pears"]
 
-  def call(env)
-    resp = Rack::Response.new
-    req = Rack::Request.new(env)
+    @@cart = []
+    
+    def call(env)
 
-    if req.path.match(/items/)
-      @@items.each do |item|
-        resp.write "#{item}\n"
-      end
-    elsif req.path.match(/search/)
-      search_term = req.params["q"]
-      resp.write handle_search(search_term)
-    else
-      resp.write "Path Not Found"
+        resp = Rack::Response.new
+        req = Rack::Request.new(env)
+
+        if req.path.match(/items/)
+
+            @@items.each do |item|
+                resp.write "#{item}\n"
+            end
+            
+        elsif req.path.match(/search/)
+            search_term = req.params["q"]
+            resp.write handle_search(search_term)
+
+        elsif req.path.match(/cart/)
+
+            if @@cart.size == 0
+
+                resp.write "Your cart is empty"
+                
+            else
+
+                @@cart.each do |item|
+
+                    resp.write "#{item}\n"
+    
+                end
+                
+            end
+        
+        elsif req.path.match(/add/)
+
+            add_item = req.params["item"]
+
+            if @@items.include? (add_item)
+
+                @@cart << add_item
+                resp.write "added #{add_item}"
+                
+            else
+
+                resp.write "We don't have that item"
+                
+            end
+            
+        else
+            resp.write "Path Not Found"
+        end
+
+        resp.finish
     end
 
-    resp.finish
-  end
-
-  def handle_search(search_term)
-    if @@items.include?(search_term)
-      return "#{search_term} is one of our items"
-    else
-      return "Couldn't find #{search_term}"
+    def handle_search(search_term)
+        
+        if @@items.include?(search_term)
+            return "#{search_term} is one of our items"
+        else
+            return "Couldn't find #{search_term}"
+        end
+        
     end
-  end
+
 end
